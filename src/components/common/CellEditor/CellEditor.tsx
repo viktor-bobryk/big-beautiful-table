13afcd41863e3299fbee96e7149411beab1e0924
viktor-bobryk <victor.bobryk@sigma.software>
2025-11-19 14:46:14 +0200

    fixed cell editor

--- src/components/common/CellEditor/CellEditor.tsx
+++ src/components/common/CellEditor/CellEditor.tsx
@@ -1,27 +1,28 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {ICellEditorParams} from 'ag-grid-community';
 import {InputText} from 'primereact/inputtext';
 import styles from './CellEditor.module.scss';
 import {getValidCellValue} from '../../../globalUtils';
+import type {CustomCellEditorProps} from 'ag-grid-react';
 
-const CellEditor: React.FC<ICellEditorParams> = (props) => {
-    const {value} = props;
-    const [currentValue, setCurrentValue] = useState<string>(value ?? '');
+const CellEditor: React.FC<ICellEditorParams> = ({value, onValueChange}: CustomCellEditorProps) => {
     const refInput = useRef<HTMLInputElement | null>(null);
 
     useEffect(() => {
-        const el = refInput.current;
-        el?.focus(); // only focus, no setSelectionRange for type="number"
+        if (refInput.current) {
+            refInput.current.focus();
+            refInput.current.select();
+        }
     }, []);
 
     return (
         <InputText
-            value={currentValue}
+            value={value}
             ref={refInput}
             type="number"
             keyfilter="int"
             className={styles['cell-editor']}
-            onChange={(event) => setCurrentValue(getValidCellValue(event.target.value))}
+            onChange={(event) => onValueChange(getValidCellValue(event.target.value))}
         />
     );
 };
